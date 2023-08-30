bee329bf7350c06a5da31c8905859db9ecf435a7
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-08-30 08:05:19 -0400

    Update TopRatedMovies.js

--- src/components/TopRatedMovies.js
+++ src/components/TopRatedMovies.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import authHeader from '../services/auth.headers';
 import { useGlobalState } from '../context/GlobalState';
-import API_URL from '../services/auth.constants';
+import { API_URL } from '../services/auth.constants';
 
 function TopRatedMovies() {
   const [topMovies, setTopMovies] = useState([]);
